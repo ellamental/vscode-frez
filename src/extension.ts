b71515fc3753643d30c5aef8ef78706654e7db73
Nick Frezynski <nick@frez.me>
2020-12-20 17:08:21 -0600

    0.0.12

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('vscode-frez.scrollDownChunk', () => {
 		// vscode.commands.executeCommand('cursorMove', {to: 'down', by: 'line', value: 10})
 		let i
+		vscode.commands.executeCommand('emacs-mcx.nextLine')
 		for (i=0; i<10; i++) {
 			vscode.commands.executeCommand('emacs-mcx.nextLine')
 		}
@@ -24,8 +25,8 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 
 	let disposable2 = vscode.commands.registerCommand('vscode-frez.scrollUpChunk', () => {
-		vscode.commands.executeCommand('cursorMove', {to: 'up', by: 'line', value: 10})
-		vscode.commands.executeCommand('editorScroll', {to: 'up', by: 'line', value: 10})
+		// vscode.commands.executeCommand('cursorMove', {to: 'up', by: 'line', value: 10})
+		// vscode.commands.executeCommand('editorScroll', {to: 'up', by: 'line', value: 10})
 		let i
 		for (i=0; i<10; i++) {
 			vscode.commands.executeCommand('emacs-mcx.prevLine')
