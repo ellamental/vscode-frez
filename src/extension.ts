677ac841afc8c62d2b1bd53b41eb9b666dae9ccd
Nick Frezynski <nick@frez.me>
2020-12-20 20:59:05 -0600

    0.0.19

--- src/extension.ts
+++ src/extension.ts
@@ -2,40 +2,48 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
+	const chunkSize: number = 10
+
 	context.subscriptions.push(vscode.commands.registerCommand('vscode-frez.scrollDownChunk', () => {
 		// The naive method below doesn't respect the mark mode in emacs-mcx.
 		// vscode.commands.executeCommand('cursorMove', {to: 'down', by: 'line', value: 10})
 		// vscode.commands.executeCommand('editorScroll', {to: 'down', by: 'line', value: 10})
 
-		let i
-		for (i=0; i<10; i++) {
+		let i: number
+		for (i=chunkSize; i<10; i++) {
 			vscode.commands.executeCommand('emacs-mcx.nextLine')
 		}
-		vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
-		// We should recenter after moving the cursor, but it makes the screen
-		// jump around when you run these scroll-chunk commands at the
-		// bottom/top of a file.
-		//
-		// Calling `emacs-mcx.recenterTopBottom` consecutively should do this,
-		// however the `emacs-mcx.nextLine` calls should "interrupt" that
-		// behavior and always result in the screen being scrolled so the cursor
-		// is "centered".  I think this is likely a "bug" in
-		// `emacs-mcx.recenterTopBottom`.
-		//
-		// vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
 
-		// vscode.commands.executeCommand('editorScroll', {to: 'down', by: 'line', value: 10})
+		// This doesn't actually "center" the cursor, but it does make the
+		// screen scroll without (visually) moving the cursor.
+		vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('vscode-frez.scrollUpChunk', () => {
-		// vscode.commands.executeCommand('cursorMove', {to: 'up', by: 'line', value: 10})
-		let i
-		for (i=0; i<10; i++) {
+		let i: number
+		for (i=chunkSize; i<10; i++) {
 			vscode.commands.executeCommand('emacs-mcx.previousLine')
 		}
 		vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
-		// vscode.commands.executeCommand('editorScroll', {to: 'up', by: 'line', value: 10})
 	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-frez.scrollUpChunkMultiple', () => {
+		let i: number
+		for (i=chunkSize*5; i<10; i++) {
+			vscode.commands.executeCommand('emacs-mcx.nextLine')
+		}
+		vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-frez.scrollDownChunkMultiple', () => {
+		let i: number
+		for (i=chunkSize*5; i<10; i++) {
+			vscode.commands.executeCommand('emacs-mcx.previousLine')
+		}
+		vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
+	}));
+
+
 
 }
 
