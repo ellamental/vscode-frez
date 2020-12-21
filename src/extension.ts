48fcf0a5830dc5dcc9fe9d9d81592385d395fdd3
Nick Frezynski <nick@frez.me>
2020-12-20 21:03:48 -0600

    0.0.20

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// vscode.commands.executeCommand('editorScroll', {to: 'down', by: 'line', value: 10})
 
 		let i: number
-		for (i=chunkSize; i<10; i++) {
+		for (i=0; i<chunkSize; i++) {
 			vscode.commands.executeCommand('emacs-mcx.nextLine')
 		}
 
@@ -21,7 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('vscode-frez.scrollUpChunk', () => {
 		let i: number
-		for (i=chunkSize; i<10; i++) {
+		for (i=0; i<chunkSize; i++) {
 			vscode.commands.executeCommand('emacs-mcx.previousLine')
 		}
 		vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
@@ -29,7 +29,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('vscode-frez.scrollUpChunkMultiple', () => {
 		let i: number
-		for (i=chunkSize*5; i<10; i++) {
+		for (i=0; i<chunkSize*5; i++) {
 			vscode.commands.executeCommand('emacs-mcx.nextLine')
 		}
 		vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
@@ -37,7 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('vscode-frez.scrollDownChunkMultiple', () => {
 		let i: number
-		for (i=chunkSize*5; i<10; i++) {
+		for (i=0; i<chunkSize*5; i++) {
 			vscode.commands.executeCommand('emacs-mcx.previousLine')
 		}
 		vscode.commands.executeCommand("emacs-mcx.recenterTopBottom")
